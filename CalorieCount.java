4041af80317895a68d0121d048309a65e1e00804
PranavG-01 <97116866+PranavG-01@users.noreply.github.com>
2022-12-01 15:55:22 -0600

    Update CalorieCount.java
    change of code, this is the right code

--- CalorieCount.java
+++ CalorieCount.java
@@ -1,20 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class CalorieCount {
-  public static void main (String[] args) {
-    Scanner stdin = new Scanner(System.in);
-    int count = 0;
-    int max = 0;
-    while (stdin.hasNext()) {
-        count += stdin.nextInt();
-    
-        if (stdin.hasNext() == "") {
-            if (count > max) {
-                max = count;
-                count = 0;
-            }
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int count = 0;
+        int max = 0;
+        String tracker;
+        while (stdin.hasNextLine()) {
+           
+           tracker = stdin.nextLine();
+           
+           if (tracker.equals("")) {
+              if (count > max) {
+                 max = count;
+                 count = 0;
+              } else {
+                 count = 0;
+              }
+              continue;
+           }
+           count += Integer.parseInt(tracker);
         }
-    }
-    System.out.print(max);
+        System.out.print(max);
+        stdin.close();
+     }
   }
-}
+  
